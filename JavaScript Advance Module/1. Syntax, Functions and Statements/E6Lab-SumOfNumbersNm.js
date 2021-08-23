04849952ab1313ba0d2b1d170b167e103f4e0d85
monisio <monisio86@gmail.com>
2021-08-23 12:48:21 +0200

    More lab problems added.

---
+++ JavaScript Advance Module/1. Syntax, Functions and Statements/E6Lab-SumOfNumbersNm.js
@@ -0,0 +1,14 @@
+function sumOfNumbers(n, m){
+    let sum = 0;
+    let num1 = Number(n);
+    let num2 = Number(m);
+
+    for (let i = num1; i <= num ; i++){
+        sum+=i;        
+      
+    }
+
+   return sum;
+}
+
+sumOfNumbers('-8', '20' );
---
+++ JavaScript Advance Module/1. Syntax, Functions and Statements/E8Lab-SquareOfStars.js
@@ -0,0 +1,18 @@
+function printStarSquare(n = 5) {
+
+
+    for (let i = 0; i < n; i++) {
+        let output = '';
+
+        for (let j = 0; j < n; j++) {
+            
+           output+='* ';
+        }
+
+        console.log(output.trimEnd());
+    }
+
+}
+
+
+printStarSquare();
