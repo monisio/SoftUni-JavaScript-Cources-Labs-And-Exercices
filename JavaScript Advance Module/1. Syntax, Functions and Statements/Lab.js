213a29ca1d145072dbd979ecad0ea5323b73ccac
monisio <monisio86@gmail.com>
2021-08-23 09:53:16 +0200

    Test file added.

---
+++ JavaScript Advance Module/1. Syntax, Functions and Statements/Lab.js
@@ -0,0 +1,14 @@
+function solve(input='String' , age ){
+    
+    
+    console.log(input.length);
+    console.log(age);
+    console.log(input);
+
+}
+
+solve(undefined, 20 );
+
+
+const val =  'yes' && null && false
+console.log(val); // null
