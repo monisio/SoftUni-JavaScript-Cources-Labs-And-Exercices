4f265a0b28eac4ea4dd45f488809e92ac5eb96a7
monisio <monisio86@gmail.com>
2021-08-23 10:24:30 +0200

    Lab problems added.

---
+++ JavaScript Advance Module/1. Syntax, Functions and Statements/E1Lab-EchoFunction.js
@@ -0,0 +1,12 @@
+function echo(input) {
+
+
+    console.log(input.length);
+    console.log(input);
+
+}
+
+echo('Hello, JavaScript!');
+
+
+
---
+++ JavaScript Advance Module/1. Syntax, Functions and Statements/E2Lab-StringLength.js
@@ -0,0 +1,12 @@
+function stringLenght(a, b, c) {
+
+    let sum = a.length + b.length + c.length;
+    let sumAverage = sum / 3;
+    console.log(sum);
+    console.log(Math.round(sumAverage));
+
+
+}
+
+
+stringLenght('chocolate', 'ice cream', 'cake');
