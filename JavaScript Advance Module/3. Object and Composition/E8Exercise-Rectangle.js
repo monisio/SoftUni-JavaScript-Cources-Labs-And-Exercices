e5f68b52ae1c6c4c5ecf7e953b8e4d8197806cec
monisio <monisio86@gmail.com>
2021-09-07 19:01:21 +0300

    Objects and composition exercise problems added.

---
+++ JavaScript Advance Module/3. Object and Composition/E8Exercise-Rectangle.js
@@ -0,0 +1,19 @@
+function rectangle(width, height, color) {
+    return {
+        width: Number(width),
+        height: Number(height),
+        color: color[0].toUpperCase() + color.slice(1),
+
+        calcArea : function(){
+            return this.width*this.height;
+        }
+    }
+
+}
+
+let rect = rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
+
