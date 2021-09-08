5bc744da7b8ebda81c014d1067f8113a1fab3336
monisio <monisio86@gmail.com>
2021-09-08 22:07:52 +0300

    DOM introduction problems added.

--- JavaScript Advance Module/4. DOM introduction/Lab/06.Sum Table/SumTable.js
+++ JavaScript Advance Module/4. DOM introduction/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,14 @@
 function sumTable() {
+    let rows = document.querySelectorAll('tr  td:nth-of-type(2n)');
+    let sum = 0;
+    for (let i = 0; i < rows.length - 1; i++) {
+        let currentRow = rows[i];
+
+        sum += Number(currentRow.textContent);
+
+
+    }
+   let lastRow= rows[rows.length-1];
+    lastRow.textContent=sum;
 
 }
