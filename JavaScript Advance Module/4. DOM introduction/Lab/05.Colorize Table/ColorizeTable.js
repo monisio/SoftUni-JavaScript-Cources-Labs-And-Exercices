c705264b00fbe8f3ed174750c5d59caa3e518cf8
monisio <monisio86@gmail.com>
2021-09-08 21:45:42 +0300

    DOM introduction problems added.

--- JavaScript Advance Module/4. DOM introduction/Lab/05.Colorize Table/ColorizeTable.js
+++ JavaScript Advance Module/4. DOM introduction/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
     // TODO
+
+    let tableElements = document.querySelectorAll("table tr:nth-of-type(2n)");
+
+    for (const tableElement of tableElements) {
+        tableElement.style.background = 'teal'
+    }
+
 }
