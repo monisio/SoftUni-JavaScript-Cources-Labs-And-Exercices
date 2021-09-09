4cfd3f2bbdfd2047e462205aafbbf61e4127859b
monisio <monisio86@gmail.com>
2021-09-09 15:55:37 +0300

    DOM introduction problems added.

--- JavaScript Advance Module/4. DOM introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JavaScript Advance Module/4. DOM introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,28 @@
 function extract(content) {
 
+    let contentElement = document.getElementById(content);
+
+    let innerString = contentElement.textContent;
+    let regex = /\((?<result>[A-Za-z ]+)\)/g;
+
+    let iter = innerString.matchAll(regex);
+
+    let result ='';
+
+    for (const regExpMatchArray of iter) {
+        result+=regExpMatchArray.groups.result+';';
+    }
+
+
+   return result;
+
+    /*  const string = "something format_abc";
+      const regexp = /(?:^|\s)format_(.*?)(?:\s|$)/g;
+      const matches = string.matchAll(regexp);
+  */
+
+
+    ///\((?<result>[A-Za-z ]+)\)/g
+
+
 }
