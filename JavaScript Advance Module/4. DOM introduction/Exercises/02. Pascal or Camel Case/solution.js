6d5cac5a979280e57d6e63ff52cd0681fbd7aa52
monisio <monisio86@gmail.com>
2021-09-09 17:02:52 +0300

    DOM introduction problems added.

--- JavaScript Advance Module/4. DOM introduction/Exercises/02. Pascal or Camel Case/solution.js
+++ JavaScript Advance Module/4. DOM introduction/Exercises/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-  //TODO...
+    //TODO...
+    let text = document.getElementById("text").value.toLowerCase().split(" ");
+    let namingConvention = document.getElementById("naming-convention").value;
+    let result = '';
+
+
+    if (namingConvention === "Camel Case") {
+        result += text[0];
+        for (let i = 1; i < text.length; i++) {
+            result += text[i].charAt(0).toUpperCase() + text[i].slice(1);
+        }
+    } else if (namingConvention === "Pascal Case") {
+
+        for (let i = 0; i < text.length; i++) {
+            result += text[i].charAt(0).toUpperCase() + text[i].slice(1);
+        }
+
+
+    } else {
+        result = 'Error!';
+    }
+
+    document.getElementById("result").textContent = result;
+
 }
