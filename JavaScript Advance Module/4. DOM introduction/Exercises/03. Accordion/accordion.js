3550ee2aa776c22f21081c8f92f3e1ceadc7e536
monisio <monisio86@gmail.com>
2021-09-10 16:07:12 +0300

    DOM introduction problems added.

--- JavaScript Advance Module/4. DOM introduction/Exercises/03. Accordion/accordion.js
+++ JavaScript Advance Module/4. DOM introduction/Exercises/03. Accordion/accordion.js
@@ -1,3 +1,17 @@
 function toggle() {
-    console.log('TODO:...');
+
+    let buttonElement = document.querySelector("span.button");
+
+    let hideShowPElement = document.getElementById("extra").style;
+
+    let buttonText = buttonElement.textContent;
+    if (buttonText === "More") {
+        hideShowPElement.display = "block";
+        buttonElement.textContent="Less";
+    } else {
+        hideShowPElement.display = "none";
+        buttonElement.textContent="More"
+    }
+
+
 }
