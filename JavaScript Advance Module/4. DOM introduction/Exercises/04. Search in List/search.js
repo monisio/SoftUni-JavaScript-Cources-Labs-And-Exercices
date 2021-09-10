e8edb4d233195ab4f928fdd979d42c3ff29751d6
monisio <monisio86@gmail.com>
2021-09-10 19:26:33 +0300

    DOM introduction problems added.

--- JavaScript Advance Module/4. DOM introduction/Exercises/04. Search in List/search.js
+++ JavaScript Advance Module/4. DOM introduction/Exercises/04. Search in List/search.js
@@ -3,17 +3,25 @@ function search() {
     let searchWord = document.getElementById("searchText").value.trim();
 
     let listElements = document.querySelectorAll("#towns li");
+    let resultElement = document.getElementById("result");
+    let arr = Array.from(listElements);
 
-    for (const listElement of listElements) {
-        let townName = listElement.textContent;
-        if (townName === searchWord) {
-            listElement.style.textDecoration = "underline";
-            listElement.style.fontWeight= "bold";
+    resultElement.textContent= arr.filter(e => e.textContent === searchWord).map(e => {
+        e.style.textDecoration = 'underline';
+        e.style.fontWeight = 'bold';
+    }).length;
+    resultElement.style.display= 'inline';
 
-        }
+    /*  for (const listElement of listElements) {
+          let townName = listElement.textContent;
+          if (townName === searchWord) {
+              listElement.style.textDecoration = "underline";
+              listElement.style.fontWeight= "bold";
+
+          }
 
 
-    }
+      }*/
 
 
 }
