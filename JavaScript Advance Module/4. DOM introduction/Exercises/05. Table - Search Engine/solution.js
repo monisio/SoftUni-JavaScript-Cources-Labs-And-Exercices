5900f89304ad72375c4730382b0e8e14529e1a87
monisio <monisio86@gmail.com>
2021-09-11 14:43:12 +0300

    DOM introduction problems added.

--- JavaScript Advance Module/4. DOM introduction/Exercises/05. Table - Search Engine/solution.js
+++ JavaScript Advance Module/4. DOM introduction/Exercises/05. Table - Search Engine/solution.js
@@ -1,8 +1,33 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+    let tableElements = document.querySelectorAll("tbody tr");
 
-   }
+
+    function onClick() {
+        //   TODO:
+
+        let searchWord = document.getElementById('searchField').value;
+        if (!searchWord) {
+            return;
+        }
+
+        let rows = Array.from(tableElements);
+        rows.forEach(e => {
+            e.className = '';
+        })
+
+        rows.forEach(e => {
+
+            let element = Array.from(e.children);
+            if (element.some(x => x.textContent.includes(searchWord))) {
+                e.className = 'select';
+            }
+
+        });
+
+
+    }
+
+
 }
