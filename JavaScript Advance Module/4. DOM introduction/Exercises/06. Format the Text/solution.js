8ced5ba5cfe4e9a337effcf1fa45e6b42709aaa2
monisio <monisio86@gmail.com>
2021-09-11 16:27:36 +0300

    DOM introduction problems added.

--- JavaScript Advance Module/4. DOM introduction/Exercises/06. Format the Text/solution.js
+++ JavaScript Advance Module/4. DOM introduction/Exercises/06. Format the Text/solution.js
@@ -1,3 +1,39 @@
 function solve() {
-  //TODO
+    //TODO
+
+    //get text from input
+    // separate text into array with split separator '.'
+    let inputElement = document.getElementById('input');
+    let inputTextAsArray = inputElement.value.split('.').filter(e => (e.length > 0)).map(e => e + '.');
+    let outputDivElement = document.getElementById("output");
+
+
+
+    let indexRoof = Math.ceil(inputTextAsArray.length / 3)
+
+
+    for (let i = 0; i < indexRoof; i++) {
+        outputDivElement.innerHTML +=("<p>" + inputTextAsArray.splice(0, 3).join("") + "</p>");
+
+    }
+
+    // let counter = 0;
+    // let currentString = '';
+    // let resultArray = [];
+    // for (let i = 0; i < inputTextAsArray.length; i++) {
+    //     counter++;
+    //     currentString += inputTextAsArray[i];
+    //
+    //
+    //     if (counter === 3) {
+    //         counter = 0;
+    //         resultArray.push("<p>" + `${currentString}` +"</p>");
+    //         currentString = '';
+    //     }
+    //
+    //
+    // }
+    //
+    // outputDivElement.innerHTML = resultArray.join('.');
+
 }
