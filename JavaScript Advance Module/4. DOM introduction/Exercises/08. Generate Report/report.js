58afcc7e97711fb2d34919bce9f5366b46233bf1
monisio <monisio86@gmail.com>
2021-09-12 19:36:21 +0300

    DOM introduction problems added.

--- JavaScript Advance Module/4. DOM introduction/Exercises/08. Generate Report/report.js
+++ JavaScript Advance Module/4. DOM introduction/Exercises/08. Generate Report/report.js
@@ -1,3 +1,29 @@
 function generateReport() {
     //TODO
+
+    let headings = document.querySelectorAll("input");
+
+
+    let resultArray = [];
+    for (let i = 0; i < headings.length; i++) {
+        let checkBoxElement = headings[i];
+        if (checkBoxElement.checked) {
+            let fieldsContentElements = document.querySelectorAll("tbody tr");
+            for (let j = 0; j < fieldsContentElements.length; j++) {
+                let obj = !resultArray[j]? {}:resultArray.shift();
+                let propertyName = checkBoxElement.name;
+                obj[propertyName] = fieldsContentElements[j].children[i].textContent;
+                resultArray.push(obj);
+            }
+
+
+        }
+
+
+    }
+
+    let output = JSON.stringify(resultArray);
+
+   document.getElementById("output").value =output;
+
 }
