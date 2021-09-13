e40c12680c785145441c9d9e8220f1eff308cdd2
monisio <monisio86@gmail.com>
2021-09-13 21:57:36 +0300

    DOM Manipulation and events problems added.

--- JavaScript Advance Module/5. Dom Manipulations and Events/Lab/01. List-Of-Items/app.js
+++ JavaScript Advance Module/5. Dom Manipulations and Events/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    //get resources
+    let itemsListElement = document.getElementById("items");
+    let inputFieldElement = document.querySelector("#newItemText");
+
+    let newItemText = inputFieldElement.value;
+
+    //create new element
+
+    let newItemElement = document.createElement("li")
+    newItemElement.appendChild(document.createTextNode(newItemText));
+
+    itemsListElement.appendChild(newItemElement);
+
+    inputFieldElement.value = '';
+
+
 }
--- JavaScript Advance Module/5. Dom Manipulations and Events/Lab/02. Add-Delete/app.js
+++ JavaScript Advance Module/5. Dom Manipulations and Events/Lab/02. Add-Delete/app.js
@@ -1,3 +1,45 @@
 function addItem() {
     //TODO...
+
+    //get resources
+    let itemsListElement = document.getElementById("items");
+    let inputFieldElement = document.querySelector("#newItemText");
+
+    let newItemText = inputFieldElement.value;
+
+    // check for empty string
+    if(newItemText.length===0){
+        return;
+    }
+
+    //create new element
+
+    let newItemElement = document.createElement("li");
+
+    //create text node in order to escape potential unwanted html text element=> same as parsing text
+
+    newItemElement.appendChild(document.createTextNode(newItemText));
+
+    // append created text element as child node of newly created element
+
+    let anchorElement = document.createElement("a");
+    let anchorText = document.createTextNode("[Delete]");
+
+    anchorElement.setAttribute("href", '#');
+
+    anchorElement.appendChild(anchorText);
+    newItemElement.appendChild(anchorElement);
+
+    itemsListElement.appendChild(newItemElement);
+
+    //if we use arrow ot lambda we cannot use function to remove only element that called the function
+    newItemElement.addEventListener('click',function(){
+        newItemElement.remove();
+        }
+    );
+
+    // clear the input field value
+    inputFieldElement.value = '';
+
+
 }
