da6f159d849aa19a081565fac548837a026711a5
monisio <monisio86@gmail.com>
2021-09-17 19:54:25 +0300

    DOM Manipulation and events problems added.

--- JavaScript Advance Module/5. Dom Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ JavaScript Advance Module/5. Dom Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,28 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttonElements = document.querySelectorAll("div button");
+    for (const buttonElement of buttonElements) {
+        buttonElement.addEventListener("click", handler);
+
+    }
+
+
+
+    function handler(event){
+
+       let profileElement = event.currentTarget.parentNode;
+       if(!profileElement.querySelector("input").checked){
+           if(profileElement.querySelector("button").textContent === "Show more"){
+               profileElement.querySelector("div").style.display="block";
+               profileElement.querySelector("button").textContent="Hide all"
+           }else {
+               profileElement.querySelector("div").style.display="none";
+               profileElement.querySelector("button").textContent="Show more"
+           }
+
+       }
+
+
+
+    }
+
 }
