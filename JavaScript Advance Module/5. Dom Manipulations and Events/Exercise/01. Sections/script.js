634cfac8146f23c9be89a6fd45e77e874056bed2
monisio <monisio86@gmail.com>
2021-09-17 16:29:24 +0300

    DOM Manipulation and events problems added.

--- JavaScript Advance Module/5. Dom Manipulations and Events/Exercise/01. Sections/script.js
+++ JavaScript Advance Module/5. Dom Manipulations and Events/Exercise/01. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+      let divContentElement = document.getElementById("content");
+
+   for (const word of words) {
+      let newDivElement = document.createElement("div");
+      let newPElement = document.createElement("p");
+      newPElement.textContent= word;
+      newPElement.style.display="none";
+      newDivElement.appendChild(newPElement);
+      newDivElement.addEventListener("click",reveal);
+
+      divContentElement.appendChild(newDivElement);
+
+   }
+
+
+   function reveal(event){
+      event.currentTarget.querySelector("p").style.display="block";
+   }
+
 }
