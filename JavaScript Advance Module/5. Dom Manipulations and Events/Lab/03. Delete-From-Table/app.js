2c1e0b250f843762c079924909b97ac14507cecc
monisio <monisio86@gmail.com>
2021-09-17 12:23:07 +0300

    DOM Manipulation and events problems added.

--- JavaScript Advance Module/5. Dom Manipulations and Events/Lab/03. Delete-From-Table/app.js
+++ JavaScript Advance Module/5. Dom Manipulations and Events/Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,28 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+
+    let inputFieldElement = document.getElementsByName("email");
+
+    let searchEmail = inputFieldElement[0].value;
+
+    inputFieldElement[0].value ='';
+
+    let tableEmailDataElements = document.querySelectorAll('tbody td:nth-child(2)');
+
+    let resultElement = document.getElementById('result');
+
+
+    for (const emailElement of  tableEmailDataElements) {
+
+        if (searchEmail===emailElement.textContent){
+            let row = emailElement.parentNode;
+            row.parentNode.removeChild(row);
+
+            resultElement.textContent="Deleted"
+            return;
+        }
+
+    }
+
+    resultElement.textContent= "Not found."
+
 }
