de554f4b09c43458ed3598bac54bff0f2b17f046
monisio <monisio86@gmail.com>
2021-09-17 13:54:34 +0300

    DOM Manipulation and events problems added.

--- JavaScript Advance Module/5. Dom Manipulations and Events/Lab/06. Dynamic-Validation/app.js
+++ JavaScript Advance Module/5. Dom Manipulations and Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+
+    let emailInputElement = document.getElementById("email");
+    emailInputElement.addEventListener("change", handler);
+
+    function handler(event) {
+        let emailEntryText = emailInputElement.value;
+
+        let result = /\w+@\w+\.\w+/g.exec(emailEntryText);
+        if(!result){
+            event.currentTarget.className= "error" ;
+        }else{
+            event.currentTarget.className="";
+        }
+
+    }
 }
