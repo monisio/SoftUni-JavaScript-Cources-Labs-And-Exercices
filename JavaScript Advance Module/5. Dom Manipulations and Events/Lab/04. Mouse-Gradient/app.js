b67138796b58dc1813eb6a4cfa0c3840499c0d0c
monisio <monisio86@gmail.com>
2021-09-17 13:01:25 +0300

    DOM Manipulation and events problems added.

--- JavaScript Advance Module/5. Dom Manipulations and Events/Lab/04. Mouse-Gradient/app.js
+++ JavaScript Advance Module/5. Dom Manipulations and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,19 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+
+    let gradientBox = document.getElementById("gradient-box");
+
+    gradientBox.addEventListener("mousemove", mouseMove);
+    gradientBox.addEventListener("mouseout", mouseOut);
+
+    function mouseMove(event) {
+        let percentage = Math.trunc((event.offsetX / (event.target.clientWidth - 1) * 100));
+        document.getElementById("result").textContent = percentage +"%";
+    }
+
+    function mouseOut(event) {
+        document.getElementById("result").textContent ='';
+
+    }
+
+
 }
