2bcf6b518da023918161f20b6bd31c569989da28
monisio <monisio86@gmail.com>
2021-09-18 18:15:45 +0300

    DOM Manipulation and events problems added.

--- JavaScript Advance Module/5. Dom Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ JavaScript Advance Module/5. Dom Manipulations and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    let selectElement = document.getElementById("menu");
+    let newItemText = document.getElementById("newItemText");
+    let newItemValue = document.getElementById("newItemValue");
+
+   let optionElement = document.createElement("option");
+
+   optionElement.textContent= newItemText.value;
+   optionElement.value= newItemValue.value;
+
+   selectElement.appendChild(optionElement);
+    newItemText.value="";
+    newItemValue.value="";
+
 }
--- JavaScript Advance Module/5. Dom Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ JavaScript Advance Module/5. Dom Manipulations and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,38 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    // add event listeners to buttons
+
+    let buttons = document.querySelectorAll("button");
+
+    buttons[0].addEventListener("click", encodeAndSent);
+    buttons[1].addEventListener("click", decodeAndRead);
+
+    function encodeAndSent(event) {
+        let divElement = event.currentTarget.parentElement;
+        let allTextAreas = document.getElementsByTagName("textarea");
+        let textArea = allTextAreas[0];
+        let recieverTextArea = allTextAreas[1];
+        let textMessage = textArea.value;
+        textArea.value = "";
+        let encodedMessage = "";
+        for (let i = 0; i < textMessage.length; i++) {
+            encodedMessage += String.fromCharCode(textMessage.charCodeAt(i) + 1);
+
+        }
+
+        recieverTextArea.value = encodedMessage;
+
+
+    }
+
+    function decodeAndRead(event) {
+        let divElement = event.currentTarget.parentElement;
+        let textArea = divElement.getElementsByTagName("textarea")[0];
+        let currentDecodedText = textArea.value;
+        let decodedMessage = "";
+        for (let i = 0; i < currentDecodedText.length ; i++) {
+            decodedMessage += String.fromCharCode(currentDecodedText.charCodeAt(i) - 1);
+        }
+        textArea.value= decodedMessage;
+    }
+
 }
