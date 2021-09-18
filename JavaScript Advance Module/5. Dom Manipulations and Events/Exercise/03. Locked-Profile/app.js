d05bba11445750f6c0da72282ea677be30e43fcd
monisio <monisio86@gmail.com>
2021-09-18 16:38:56 +0300

    DOM Manipulation and events problems added.

--- JavaScript Advance Module/5. Dom Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ JavaScript Advance Module/5. Dom Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -6,23 +6,56 @@ function lockedProfile() {
     }
 
 
+    function handler(event) {
 
-    function handler(event){
+        let profileElement = event.currentTarget.parentElement;
+        let unlockButton = profileElement.querySelector("input:checked");
+        if (unlockButton.value !== "lock") {
+            if (profileElement.querySelector("button").textContent === "Show more") {
+                profileElement.querySelector("div").style.display = "inline-block";
+                profileElement.querySelector("button").textContent = "Hide all"
+            } else {
+                profileElement.querySelector("div").style.display = "none";
+                profileElement.querySelector("button").textContent = "Show more"
+            }
 
-       let profileElement = event.currentTarget.parentNode;
-       if(!profileElement.querySelector("input").checked){
-           if(profileElement.querySelector("button").textContent === "Show more"){
-               profileElement.querySelector("div").style.display="block";
-               profileElement.querySelector("button").textContent="Hide all"
-           }else {
-               profileElement.querySelector("div").style.display="none";
-               profileElement.querySelector("button").textContent="Show more"
-           }
+        }
 
-       }
+
 
 
 
     }
+
+
+    /*function lockedProfile() {
+    let buttonElements = document.querySelectorAll("div button");
+    for (const buttonElement of buttonElements) {
+        buttonElement.addEventListener("click", handler);
+
+    }
+
+
+    function handler(event) {
+
+        let profileElement = event.currentTarget.parentElement;
+        let unlockButton = profileElement.querySelector("input:checked");
+        if (unlockButton.value !== "lock") {
+            if (profileElement.querySelector("button").textContent === "Show more") {
+                profileElement.querySelector("div").style.display = "inline-block";
+                profileElement.querySelector("button").textContent = "Hide all"
+            } else {
+                profileElement.querySelector("div").style.display = "none";
+                profileElement.querySelector("button").textContent = "Show more"
+            }
+
+        }
+
+
+    }
+
+
+}*/
+
 
 }
