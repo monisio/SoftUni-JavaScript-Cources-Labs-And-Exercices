7bf7d4dac7f7cf657be6e944e5115ccba5bf949e
monisio <monisio86@gmail.com>
2021-09-20 19:16:07 +0300

    DOM Manipulation and events problems added.

---
+++ JavaScript Advance Module/6.Advanced Functions/E04Lab-FilterEmployee.js
@@ -0,0 +1,40 @@
+function filter(persons, criteria) {
+
+    let personsAsArray = JSON.parse(persons);
+    let [filterBy, data] = criteria.split("-");
+
+
+    personsAsArray.filter(e => e[filterBy] === data)
+        .map((e, i) => console.log(`${i}. ${e.first_name} ${e.last_name} - ${e.email}`
+            .trim()));
+
+}
+
+
+filter(`[{
+    "id": "1",
+    "first_name": "Kaylee",
+    "last_name": "Johnson",
+    "email": "k0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Johnson",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  }, {
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }, {
+    "id": "4",
+    "first_name": "Evanne",
+    "last_name": "Johnson",
+    "email": "ev2@hostgator.com",
+    "gender": "Male"
+  }]`,
+    'last_name-Johnson'
+);
