56d9e2b1f293dc827e5e053b032d1c8ea1c316ee
monisio <monisio86@gmail.com>
2021-09-21 17:43:57 +0300

    Advanced Functions problems added.

--- JavaScript Advance Module/6.Advanced Functions/E03Exercise-.js
+++ JavaScript Advance Module/6.Advanced Functions/E03Exercise-.js
@@ -1,16 +1,16 @@
 function fibonator() {
     let num1 = 0;
     let num2 = 1;
-    let firstCall= true;
+    let firstCall = true;
     return function () {
-        if (firstCall){
-            firstCall= false;
+        if (firstCall) {
+            firstCall = false;
             return 1;
         }
         let prev = num2;
-        let result = num1 + num2
+        num2 = num1 + num2
         num1 = prev;
-        num2 = result;
+
 
         return num2;
     }
