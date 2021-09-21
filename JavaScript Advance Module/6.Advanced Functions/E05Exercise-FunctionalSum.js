7980efd0ef495faf2873b9da6135d70b5b9de15c
monisio <monisio86@gmail.com>
2021-09-21 21:42:59 +0300

    Advanced Functions problems added.

---
+++ JavaScript Advance Module/6.Advanced Functions/E05Exercise-FunctionalSum.js
@@ -0,0 +1,18 @@
+function add(num) {
+    let sum = num;
+
+
+    function calc(num2) {
+        sum += num2;
+        return calc;
+    }
+
+    calc.toString = function () {
+        return sum;
+    }
+    return calc;
+}
+
+
+console.log(add(3)(3)(5).toString());
+
