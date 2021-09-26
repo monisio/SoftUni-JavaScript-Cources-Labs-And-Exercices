af90e1505eded7847a12afbe665bd84c44ff7949
monisio <monisio86@gmail.com>
2021-09-26 16:45:05 +0300

    Classes problems added.

---
+++ JavaScript Advance Module/8. Classes/E5Exercise-LengthLimit.js
@@ -0,0 +1,46 @@
+class Stringer {
+
+    constructor(string, lenght) {
+        this.innerString = string;
+        this.innerLength = lenght;
+        this.lengthBelowZero();
+    }
+
+    increase(length) {
+        this.innerLength += length;
+    }
+
+    decrease(length) {
+        this.innerLength-= length;
+        this.lengthBelowZero();
+    }
+
+    lengthBelowZero() {
+        if (this.innerLength < 0) {
+            this.innerLength = 0;
+        }
+    }
+
+    toString(){
+        let output = "";
+        if(this.innerString.length>this.innerLength){
+            output+= this.innerString.substring(0, this.innerLength)+"...";
+        }else{
+            output= this.innerString;
+        }
+        return output;
+    }
+}
+
+
+let test = new Stringer("Test", 5);
+console.log(test.toString()); // Test
+
+test.decrease(3);
+console.log(test.toString()); // Te...
+
+test.decrease(5);
+console.log(test.toString()); // ...
+
+test.increase(4);
+console.log(test.toString()); // Test
