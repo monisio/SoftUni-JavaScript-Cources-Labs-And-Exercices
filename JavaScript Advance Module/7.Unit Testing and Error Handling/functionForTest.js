16a53a3f4df0f3e0b382933d40cfed752169230d
monisio <monisio86@gmail.com>
2021-09-30 16:52:26 +0300

    Unit Testing and Error Handling problems added.

---
+++ JavaScript Advance Module/7.Unit Testing and Error Handling/functionForTest.js
@@ -0,0 +1,8 @@
+function sum(a,b){
+    return a+b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+module.exports= {sum , divide};
---
+++ JavaScript Advance Module/7.Unit Testing and Error Handling/test/Tests.js
@@ -0,0 +1,7 @@
+let expect = require("chai");
+
+let sum =require("../functionForTest").sum;
+let divide = require("../functionForTest").divide;
+
+console.log(sum(4,4));
+console.log(divide)
