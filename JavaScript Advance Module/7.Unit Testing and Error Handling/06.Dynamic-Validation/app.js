5e21d5e423e59dc5c512ab25ca06199cbbb3df5c
monisio <monisio86@gmail.com>
2021-10-03 14:58:07 +0300

    Unit Testing and Error Handling problems added.

--- JavaScript Advance Module/7.Unit Testing and Error Handling/06.Dynamic-Validation/app.js
+++ JavaScript Advance Module/7.Unit Testing and Error Handling/06.Dynamic-Validation/app.js
@@ -1,3 +1,10 @@
 function validate() {
-    // TODO
+    let inputElement = document.getElementById("email");
+    inputElement.addEventListener("change",()=>{
+        if(!/[a-z]+@[a-z]+\.[a-z]+/g.test(inputElement.value)){
+            inputElement.className="error";
+        } else{
+            inputElement.className="";
+        }
+    });
 }
