08c77971766673e996a9d4278112bbd0b9510078
monisio <monisio86@gmail.com>
2021-10-07 21:47:38 +0300

    Asynchronous programming lab problems added.

--- JavaScript Applications Module/1. Asynchronous Programming/01.XHR/scripts/app.js
+++ JavaScript Applications Module/1. Asynchronous Programming/01.XHR/scripts/app.js
@@ -1,3 +1,18 @@
 function loadRepos() {
-   console.log("TODO...");
+    let resultElement = document.getElementById("res");
+    let url = "https://api.github.com/users/testnakov/repos";
+
+    const httpRequest = new XMLHttpRequest();
+    httpRequest.addEventListener("readystatechange", function () {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+            resultElement.textContent = httpRequest.responseText;
+        } else {
+            resultElement.textContent = "Error fetching data!"
+        }
+
+    });
+
+    httpRequest.open("GET", url);
+    httpRequest.send();
+
 }
