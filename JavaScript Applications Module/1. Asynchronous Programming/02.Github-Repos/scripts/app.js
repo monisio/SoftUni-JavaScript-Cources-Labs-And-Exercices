a91e7a13a4fb990823f947cc0bca40573d673257
monisio <monisio86@gmail.com>
2021-10-08 13:56:25 +0300

    Asynchronous programming lab problems added.

--- JavaScript Applications Module/1. Asynchronous Programming/02.Github-Repos/scripts/app.js
+++ JavaScript Applications Module/1. Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,22 @@
 function loadRepos() {
-	console.log("TODO...");
+
+    let username = "https://api.github.com/users/" + document.getElementById("username").value + "/repos";
+    let reposElement = document.getElementById("repos");
+    reposElement.removeChild(reposElement.children[0]);
+
+    fetch(username)
+        .then((responce) => responce.json())
+        .then(data => {
+
+            for (let i = 0; i < data.length; i++) {
+                let liNewElement = document.createElement("li");
+                let aNewElement = document.createElement("a");
+                aNewElement.textContent = data[i].full_name;
+                aNewElement.href = data[i].html_url;
+                liNewElement.appendChild(aNewElement);
+                reposElement.appendChild(liNewElement);
+            }
+
+        }).catch(error => reposElement.textContent = error.message);
+
 }
