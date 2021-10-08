bc5e529c56faeb44f3035fa06fd922fcc5a35704
monisio <monisio86@gmail.com>
2021-10-08 15:23:10 +0300

    Asynchronous programming lab problems added.

--- JavaScript Applications Module/1. Asynchronous Programming/03.Gighub-Commits/app.js
+++ JavaScript Applications Module/1. Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,29 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let commitsResultElement = document.getElementById("commits");
+    let username = document.getElementById("username").value;
+    let repository = document.getElementById("repo").value;
+
+    let url = `https://api.github.com/repos/${username}/${repository}/commits`
+
+
+    fetch(url).then((response)=>{
+      if(!response.ok){
+          throw new Error("Error"+response.status +response.statusText);
+      }else{
+        return   response.json()
+      }
+    }).then((result)=> {
+        for (let i = 0; i <result.length; i++) {
+          let newElement =  document.createElement("li");
+          newElement.textContent= result[i].commit.author.name +" : "+ result[i].commit.message;
+          commitsResultElement.appendChild(newElement);
+        }
+
+    }).catch(error=> {
+        let  errorMessageElement = document.createElement("li");
+        errorMessageElement.textContent= error.message;
+        commitsResultElement.appendChild(errorMessageElement);
+    })
+
+
 }
