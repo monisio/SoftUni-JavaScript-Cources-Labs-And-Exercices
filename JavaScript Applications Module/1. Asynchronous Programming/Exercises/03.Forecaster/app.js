3ef657ba04d21668b5a77ea783d6696e0fd68715
monisio <monisio86@gmail.com>
2021-10-12 16:32:28 +0300

    Asynchronous programming lab problems added.

--- JavaScript Applications Module/1. Asynchronous Programming/Exercises/03.Forecaster/app.js
+++ JavaScript Applications Module/1. Asynchronous Programming/Exercises/03.Forecaster/app.js
@@ -5,17 +5,24 @@ function attachEvents() {
 
     document.getElementById("submit").addEventListener("click", () => {
         let userInputLocation = document.getElementById("location").value;
+
+
         fetch("http://localhost:3030/jsonstore/forecaster/locations")
             .then(response => response.json())
             .then(locations => {
                 let location = locations.find(x => x.name === userInputLocation);
                 return fetch(`http://localhost:3030/jsonstore/forecaster/today/${location.code}`)
+                    .then(body => body.json())
+                    .then(currentWeatherReport => ({code: location.code, currentWeatherReport}));
             })
-            .then(body => body.json())
-            .then(currentWeatherReport => {
+            .then(({code, currentWeatherReport}) => {
                 createCurrentWeather(currentWeatherReport)
-                return fetch(`http://localhost:3030/jsonstore/forecaster/upcoming`)
-            })
+                return fetch(`http://localhost:3030/jsonstore/forecaster/upcoming/` + code);
+            }).then(body => body.json())
+            .then(result => {
+                result.forecast.forEach(dayReport => createThreeDayForecast(dayReport));
+
+            }).catch(error => error)
 
 
     });
@@ -27,7 +34,36 @@ let conditions = {
     "Overcast": "☁",
     "Rain": "☂",
 
+};
+
+
+function createThreeDayForecast(report) {
+    let upcomingDiv = document.getElementById("upcoming");
+
+    let divForecast = document.createElement("div");
+    divForecast.classList.add("forecast-info");
+
+    let mainSpan = document.createElement("span");
+    mainSpan.classList.add("upcoming");
+
+    let symbolSpan = document.createElement("span");
+    symbolSpan.classList.add("symbol");
+    symbolSpan.textContent = conditions[report.condition];
+    mainSpan.appendChild(symbolSpan);
+
+    let forecastTemp = document.createElement("span");
+    forecastTemp.classList.add("forecast-data");
+    forecastTemp.textContent = `${report.low} °/${report.high} °`
+    mainSpan.appendChild(forecastTemp);
+
+    let forecastType = document.createElement("span");
+    forecastType.classList.add("forecast-data");
+    forecastType.textContent = report.condition;
+    mainSpan.appendChild(forecastType);
+    upcomingDiv.appendChild(mainSpan);
+
 }
+
 
 function createCurrentWeather(report) {
 
@@ -37,8 +73,8 @@ function createCurrentWeather(report) {
     forecastDiv.classList.add("forecasts");
 
     let conditionSymbolSpan = document.createElement("span");
-    conditionSymbolSpan.classList.add("condition" ,"symbol");
-    conditionSymbolSpan.textContent= conditions[report.forecast.condition];
+    conditionSymbolSpan.classList.add("condition", "symbol");
+    conditionSymbolSpan.textContent = conditions[report.forecast.condition];
 
 
     let mainSpan = document.createElement("span");
@@ -46,15 +82,15 @@ function createCurrentWeather(report) {
 
     let locationSpan = document.createElement("span");
     locationSpan.classList.add("forecast-data");
-    locationSpan.textContent= report.name
+    locationSpan.textContent = report.name
 
     let degreeSpan = document.createElement("span");
     degreeSpan.classList.add("forecast-data");
-    degreeSpan.textContent=`${report.forecast.low} °/${report.forecast.high} °`
+    degreeSpan.textContent = `${report.forecast.low} °/${report.forecast.high} °`
 
     let conditionSpan = document.createElement("span");
     conditionSpan.classList.add("forecast-data");
-    conditionSpan.textContent= report.forecast.condition;
+    conditionSpan.textContent = report.forecast.condition;
 
 
     forecastDiv.appendChild(conditionSymbolSpan);
@@ -63,7 +99,7 @@ function createCurrentWeather(report) {
     mainSpan.appendChild(conditionSpan);
     forecastDiv.appendChild(mainSpan);
     currentConditionDiv.appendChild(forecastDiv);
-    currentConditionDiv.parentElement.style.display="block";
+    currentConditionDiv.parentElement.style.display = "block";
 }
 
 attachEvents();
