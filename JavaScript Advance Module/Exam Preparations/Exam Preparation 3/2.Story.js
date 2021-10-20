ae96abb996d47ea4939b9e43bc940bf4dc938f2d
monisio <monisio86@gmail.com>
2021-10-20 16:31:43 +0300

    problems added

--- JavaScript Advance Module/Exam Preparations/Exam Preparation 3/2.Story.js
+++ JavaScript Advance Module/Exam Preparations/Exam Preparation 3/2.Story.js
@@ -16,7 +16,7 @@ class Story {
             output = `${this.title} has 0 likes`;
         } else if (this._likes.length === 1) {
             //define likes-bind username !!!
-            output = `${this._likes[0]} likes the story!`
+            output = `${this._likes[0]} likes this story!`
         } else {
             output = `${this._likes[0]} and ${this._likes.length - 1} others like this story!`
         }
@@ -36,15 +36,16 @@ class Story {
         } else {
             this._likes.push(username);
         }
-
+        return `${username} liked ${this.title}!`
     }
 
     dislike(username) {
 
-        let output = this._likes.splice(this._likes.indexOf(username), 1);
-        if (output.length === 0) {
+        let index = this._likes.indexOf(username)
+        if (index === -1) {
             throw new Error("You can't dislike this story!")
         } else {
+            this._likes.splice(index, 1)
             return `${username} disliked ${this.title}`
         }
     }
@@ -52,25 +53,25 @@ class Story {
 
     comment(username, content, id) {
         let output = "";
-        let existingComment = this._comments[Number(id) - 1]// this._comments[Number(id) - 1]: undefined ;
+        let existingComment = this._comments.find(c=> c.id === id);
         if (!existingComment) {
             this._comments.push({
                 username: username,
-                id: (this._comments.length+1),
+                id: (this._comments.length + 1),
                 content: content,
                 replies: []
             });
 
-            output= `${username} commented on ${this.title}`;
+            output = `${username} commented on ${this.title}`;
         } else {
             existingComment["replies"].push({
-                id: Number((this._comments.indexOf(existingComment)+1) +"."+ (existingComment["replies"].length + 1)),
+                id: Number((this._comments.indexOf(existingComment) + 1) + "." + (existingComment["replies"].length + 1)),
                 username: username,
                 content: content,
 
             })
 
-            output= "You replied successfully"
+            output = "You replied successfully"
         }
 
         return output;
@@ -79,10 +80,10 @@ class Story {
 
 
     toString(sortingType) {
-        const sortingTypes={
-            asc: (a,b)=>  a.id-b.id ,
-            desc:(a,b)=> b.id-a.id,
-            username:(a,b)=>  a.username.localeCompare(b.username),
+        const sortingTypes = {
+            asc: (a, b) => a.id - b.id,
+            desc: (a, b) => b.id - a.id,
+            username: (a, b) => a.username.localeCompare(b.username),
 
         }
 
@@ -90,30 +91,16 @@ class Story {
 
         result.push(`Title: ${this.title}`);
         result.push(`Creator: ${this.creator}`);
-        result.push(`likes: ${this._likes.length}`);
+        result.push(`Likes: ${this._likes.length}`);
         result.push("Comments:")
-        this._comments.sort((a,b)=> sortingTypes[sortingType](a,b)).forEach(c=>{
-            result.push(`-- ${c.id}. ${c.username}: ${c.content}`)
-            c.replies.sort((a,b)=> sortingTypes[sortingType](a,b)).forEach(r=> result.push(`--- ${r.id}. ${r.username}: ${r.content}`))
-        })
+        if(this._comments.length>0){
+            this._comments.sort((a, b) => sortingTypes[sortingType](a, b)).forEach(c => {
+                result.push(`-- ${c.id}. ${c.username}: ${c.content}`)
+                c.replies.sort((a, b) => sortingTypes[sortingType](a, b)).forEach(r => result.push(`--- ${r.id}. ${r.username}: ${r.content}`))
+            })
+        }
+
 
         return result.join("\n");
     }
 }
-
-
-let art = new Story("My Story", "Anny");
-art.like("John");
-console.log(art.likes);
-art.dislike("John");
-console.log(art.likes);
-art.comment("Sammy", "Some Content");
-console.log(art.comment("Ammy", "New Content"));
-art.comment("Zane", "Reply", 1);
-art.comment("Jessy", "Nice :)");
-console.log(art.comment("SAmmy", "Reply@", 1));
-console.log()
-console.log(art.toString('username'));
-console.log()
-art.like("Zane");
-console.log(art.toString('desc'));
